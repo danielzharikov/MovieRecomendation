4ac3df476ff481257bcde5819d4604c0754d5fea
dszharikov@yandex.ru <dszharikov@yandex.ru>
2020-09-29 20:23:44 +0300

    AddedLoadOfMoviesAndImdbID

---
+++ DataMovie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataMovie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
